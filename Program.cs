f7947aa89dc5a93b2690bae68418249343f2ba89
ilya <ilia.arkhangelsky@gmail.com>
2022-12-16 17:36:12 +0300

    Рабочая версия программы, которая выдает чётные числа от 1 до N

--- Program.cs
+++ Program.cs
@@ -1,2 +1,22 @@
 ﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
 
+Console.Write("Введите число: ");
+string userInput = Console.ReadLine () ?? "";
+
+int number_N = int.Parse(userInput);
+int count = 1;
+
+while (count < number_N)
+{
+    if (count % 2 == 0)
+    {
+        Console.Write(count);
+        Console.Write(" ");
+        count = count + 1;
+    }
+    else
+    {
+        count = count + 1;
+    }
+}
+Console.Write(number_N);
